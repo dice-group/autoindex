df7a2d79aa691d7bd0fbf018d92afdd06360233f
Kunal <kunal94jha@gmail.com>
2016-05-31 16:41:39 +0200

    Fixed bugs

--- autoindex/src/main/java/org/aksw/simba/dbpedia/search/SearchLuceneLabel.java
+++ autoindex/src/main/java/org/aksw/simba/dbpedia/search/SearchLuceneLabel.java
@@ -10,8 +10,10 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.logging.Logger;
 import java.net.URLDecoder;
 import org.apache.lucene.store.NIOFSDirectory;
+import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -35,11 +37,11 @@ import org.apache.lucene.search.SortedNumericSortField;
 
 public class SearchLuceneLabel {
 	final static int TIMES_MORE_RESULTS = 10;
-
+	private static org.slf4j.Logger log = LoggerFactory.getLogger(SearchLuceneLabel.class);
 	public DirectoryReader readerFromIndex(NIOFSDirectory dir) throws IOException {
 		return DirectoryReader.open(dir);
 	}
-
+	static boolean flag = false;
 	public BooleanQuery queryFromString(String queryString) throws UnsupportedEncodingException {
 		BooleanQuery query = new BooleanQuery();
 		PhraseQuery phraseQuery = new PhraseQuery();
@@ -80,59 +82,60 @@ public class SearchLuceneLabel {
 		return res;
 	}
 
-	public static List<Result>  getResut(String index, String term) throws IOException {
+	public static List<Result> getResut(String index, String term) throws IOException {
 		Properties prop = new Properties();
 		InputStream input = new FileInputStream("src/main/java/properties/autoindex.properties");
 		prop.load(input);
 		IndexSearcher searcher = null;
-		
+		List<Result> resultlist = null;
 		switch (index.toUpperCase()) {
-		case "CLASS": 
-		{
-			
+		case "CLASS": {
+
 			String indexDir = prop.getProperty("index_class");
-			
+
 			@SuppressWarnings("deprecation")
 			IndexReader reader = IndexReader.open(NIOFSDirectory.open(new File(indexDir)));
 			searcher = new IndexSearcher(reader);
-			
 
 			break;
-			}
+		}
 		case "INSTANCE":
-		
+
 		{
-			
-			String indexDir = prop.getProperty("index_instence");
-			
+
+			String indexDir = prop.getProperty("index_instance");
+
 			@SuppressWarnings("deprecation")
 			IndexReader reader = IndexReader.open(NIOFSDirectory.open(new File(indexDir)));
 			searcher = new IndexSearcher(reader);
-			break;}
-		case "PROPERTIES":
-		{
-			String indexDir = prop.getProperty("index_properties");
-			
+			break;
+		}
+		case "PROPERTY": {
+			String indexDir = prop.getProperty("index_property");
+
 			@SuppressWarnings("deprecation")
 			IndexReader reader = IndexReader.open(NIOFSDirectory.open(new File(indexDir)));
 			searcher = new IndexSearcher(reader);
 			break;
 		}
 		default:
+			String indexDir = prop.getProperty("index_instance");
+
+			@SuppressWarnings("deprecation")
+			IndexReader reader = IndexReader.open(NIOFSDirectory.open(new File(indexDir)));
+			searcher = new IndexSearcher(reader);
+			flag=true;
 			break;
+
 		}
 		
-		
+
 		SearchLuceneLabel tester;
-		List<Result> resultlist=null;
+
 		try {
 			tester = new SearchLuceneLabel();
 
-			 resultlist = tester.search(searcher, term, 0);
-			
-
-			
-			
+			resultlist = tester.search(searcher, term, 0);
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -144,14 +147,22 @@ public class SearchLuceneLabel {
 		Handler.generateIndexforClass();
 		Handler.generateIndexforProperties();
 		Handler.generateIndexforInstances();
-		
+
 		port(8080);
 		Gson gson = new GsonBuilder().create();
-		
+
 		get("/search", (req, res) -> {
 			String index = req.queryParams("Index");
 			String searchlabel = req.queryParams("term");
-			return gson.toJson(getResut(index, searchlabel));
+			List<Result> query_result = getResut(index, searchlabel);
+			log.info("Responding to Query");
+			if (flag==true) {
+				log.info("Choosing default index");
+				flag=false;
+				return gson.toJson("RETURNING THE DEFAULT SEARCH                                  " +query_result );
+				
+			} else
+				return gson.toJson(query_result);
 		});
 
 	}
