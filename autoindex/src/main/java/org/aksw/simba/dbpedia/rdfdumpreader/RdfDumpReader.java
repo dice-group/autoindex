40d1c0f5c5be88d4d72c4ccb5e30e43b7c391e0b
Kunal <kunal94jha@gmail.com>
2016-06-06 17:11:13 +0200

    Added the modified Stream Reader

--- autoindex/src/main/java/org/aksw/simba/dbpedia/rdfdumpreader/RdfDumpReader.java
+++ autoindex/src/main/java/org/aksw/simba/dbpedia/rdfdumpreader/RdfDumpReader.java
@@ -1,30 +1,80 @@
 package org.aksw.simba.dbpedia.rdfdumpreader;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.lang.CollectorStreamTriples;
+import org.apache.jena.riot.system.StreamRDF;
+import org.apache.jena.riot.system.StreamRDFLib;
+
+import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.sparql.core.Quad;
 import com.hp.hpl.jena.util.FileManager;
 
 public class RdfDumpReader {
 	static Set<Resource> listofResources;
 
-	public static CollectorStreamTriples getResource() {
+	public static ArrayList<Node> getResource() {
 		FileManager.get().addLocatorClassLoader(RdfDumpReader.class.getClassLoader());
-		CollectorStreamTriples inputStream = new CollectorStreamTriples();
-		RDFDataMgr.parse(inputStream, "pagerank_en_2015-10.ttl");
-		return inputStream;
-
+		StreamRDFLib lib= new StreamRDFLib();
+		
+		final ArrayList<Node> list = new ArrayList<>();
+		StreamRDF destination = new StreamRDF() {
+			
+			@Override
+			public void triple(Triple triple) {
+				// TODO Auto-generated method stub
+				if(!triple.getSubject().isBlank())	
+				list.add(triple.getSubject());
+			}
+			
+			@Override
+			public void start() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void quad(Quad quad) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void prefix(String prefix, String iri) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void finish() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void base(String base) {
+				// TODO Auto-generated method stub
+				
+			}
+		}  ;
+		RDFDataMgr.parse(destination, "C:\\Users\\Kunal\\workspace\\New folder\\instance_types_en.ttl");
+		
+		return list;
+		
 	}
 
 	public static void main(String[] args) {
+		
+		
 
-		CollectorStreamTriples inputStream = getResource();
-		for (Triple triple : inputStream.getCollected()) {
+		ArrayList<Node> inputStream = getResource();
+		for (Node triple : inputStream) {
 
-			System.out.println(triple);
+			System.out.println(triple.getURI());
 		}
 
 	}
