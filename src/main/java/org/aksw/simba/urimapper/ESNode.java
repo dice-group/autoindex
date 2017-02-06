3c844088d3ac5f1bdff8480ec5ba4a5f3f8d4bee
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2017-02-06 13:17:54 +0100

    Fixed compiling bug

--- src/main/java/org/aksw/simba/urimapper/ESNode.java
+++ src/main/java/org/aksw/simba/urimapper/ESNode.java
@@ -1,5 +1,7 @@
 package org.aksw.simba.urimapper;
 
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -16,12 +18,14 @@ import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.ByteSizeUnit;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeValidationException;
 import org.elasticsearch.search.SearchHit;
 import org.openrdf.model.Statement;
 import org.openrdf.rio.RDFParser;
@@ -29,8 +33,6 @@ import org.openrdf.rio.helpers.AbstractRDFHandler;
 import org.openrdf.rio.turtle.TurtleParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
 
 public class ESNode implements IndexerInterface {
 	private static Logger log = LoggerFactory.getLogger(ESNode.class);
@@ -42,88 +44,93 @@ public class ESNode implements IndexerInterface {
 	private BulkProcessor bulkProcessor;
 
 	public void startCluster(String clustername) {
-//		/* Initialization of cluster */
-//		  Settings settings = Settings.builder()
-//		            .put("path.home",clustername)
-//		            .put("transport.type", "local")
-//		            .put("http.enabled", false)
-//		            .build();
-//
-//		   try {
-//	        this.node = new Node(settings).start();
-//        } catch (NodeValidationException e) {
-//	        // TODO Auto-generated catch block
-//	        e.printStackTrace();
-//        }
+		// /* Initialization of cluster */
+		 Settings settings = Settings.builder()
+				 .put("index.analysis.analyzer.my_ngram_analyzer.type", "custom")
+                 .put("index.analysis.analyzer.my_ngram_analyzer.tokenizer", "my_ngram_tokenizer")
+                 .put("index.analysis.tokenizer.my_ngram_tokenizer.type", "nGram")
+                 .put("index.analysis.tokenizer.my_ngram_tokenizer.min_gram", "1")
+                 .put("index.analysis.tokenizer.my_ngram_tokenizer.max_gram", "10")
+                 .putArray("index.analysis.tokenizer.my_ngram_tokenizer.token_chars", new String[0]).build();
 		
-		 this.node = nodeBuilder().clusterName(clustername).settings(ImmutableSettings.settingsBuilder().put("index.analysis.analyzer.my_ngram_analyzer.type", "custom")
-		                .put("index.analysis.analyzer.my_ngram_analyzer.tokenizer", "my_ngram_tokenizer")
-		                .put("index.analysis.tokenizer.my_ngram_tokenizer.type", "nGram")
-		                .put("index.analysis.tokenizer.my_ngram_tokenizer.min_gram", "1")
-		                .put("index.analysis.tokenizer.my_ngram_tokenizer.max_gram", "10")
-		                .putArray("index.analysis.tokenizer.my_ngram_tokenizer.token_chars", new String[0]))
-				    .node();
+		 try {
+		 this.node = new Node(settings).start();
+		 } catch (NodeValidationException e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+		 }
+
 
 		/* Starting the central server */
 		this.client = this.node.client();
 
-
 		/* Base URI for Parsing */
 		this.baseURI = "http://dbpedia.org";
 
-		//ONLY WORKS in LOCAL indizes
-//		if (!(new File(clustername).exists())) {
-		//TODO proper checking if local index exists
+		// ONLY WORKS in LOCAL indizes
+		// if (!(new File(clustername).exists())) {
+		// TODO proper checking if local index exists
 
-			/* Prepare Bulk Load */
-			this.bulkProcessor = BulkProcessor.builder(client, new BulkProcessor.Listener() {
-				@Override
-				public void beforeBulk(long executionId, BulkRequest request) {
-					log.debug("Before Bulk");
-				}
+		/* Prepare Bulk Load */
+		this.bulkProcessor = BulkProcessor.builder(client, new BulkProcessor.Listener() {
+			@Override
+			public void beforeBulk(long executionId, BulkRequest request) {
+				log.debug("Before Bulk");
+			}
 
-				@Override
-				public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
-					log.debug("After Bulk. Bulk took: " + response.getTookInMillis());
-				}
+			@Override
+			public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+				log.debug("After Bulk. Bulk took: " + response.getTookInMillis());
+			}
 
-				@Override
-				public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
-					log.error("After Bulk", failure);
-				}
-			}).setBulkActions(100000).setBulkSize(new ByteSizeValue(1, ByteSizeUnit.GB)).setFlushInterval(TimeValue.timeValueSeconds(60)).setConcurrentRequests(0).build();
-//		}
-			
+			@Override
+			public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+				log.error("After Bulk", failure);
+			}
+		})
+		                                  .setBulkActions(100000)
+		                                  .setBulkSize(new ByteSizeValue(1, ByteSizeUnit.GB))
+		                                  .setFlushInterval(TimeValue.timeValueSeconds(60))
+		                                  .setConcurrentRequests(0)
+		                                  .build();
+		// }
+
 	}
 
 	public void rdfcluster(String labelspath, String indexname) throws FileNotFoundException, IOException {
-		//TODO proper checking if local index exists
+		// TODO proper checking if local index exists
 
-//		if (bulkProcessor!=null) {
-			/* Index name */
-			this.indexname = indexname;
+		// if (bulkProcessor!=null) {
+		/* Index name */
+		this.indexname = indexname;
 
-			/* Parsing turtlefile */
-			log.info("Start parsing: " + labelspath);
-			RDFParser parser = new TurtleParser();
-			OnlineStatementHandler osh = new OnlineStatementHandler();
-			parser.setRDFHandler(osh);
-			parser.parse(new FileReader(labelspath), baseURI);
-			log.info("Finished parsing: " + labelspath);
-//		}
+		/* Parsing turtlefile */
+		log.info("Start parsing: " + labelspath);
+		RDFParser parser = new TurtleParser();
+		OnlineStatementHandler osh = new OnlineStatementHandler();
+		parser.setRDFHandler(osh);
+		parser.parse(new FileReader(labelspath), baseURI);
+		log.info("Finished parsing: " + labelspath);
+		// }
 	}
 
 	private void addToIndex(String subject, String objectString) throws IOException {
 		/* Indexing the data in the central server */
-		bulkProcessor.add(new IndexRequest(indexname, "mappings").source(jsonBuilder().startObject().field("uri", subject).field("label", objectString).endObject()));
+		bulkProcessor.add(new IndexRequest(indexname, "mappings").source(jsonBuilder().startObject()
+		                                                                              .field("uri", subject)
+		                                                                              .field("label", objectString)
+		                                                                              .endObject()));
 	}
 
 	private class OnlineStatementHandler extends AbstractRDFHandler {
 		@SuppressWarnings("unused")
 		public void handleStatement(Statement st) {
-			String subject = st.getSubject().stringValue();
-			String predicate = st.getPredicate().stringValue();
-			String object = st.getObject().stringValue();
+			String subject = st.getSubject()
+			                   .stringValue();
+			String predicate = st.getPredicate()
+			                     .stringValue();
+			String object = st.getObject()
+			                  .stringValue();
 			try {
 				addToIndex(subject, object);
 			} catch (IOException e) {
@@ -166,9 +173,14 @@ public class ESNode implements IndexerInterface {
 						for (String label : labelarr) {
 							i = i + 1;
 							client.prepareIndex(indexname, "mappings", String.valueOf(i))
-							        .setSource(
-							                jsonBuilder().startObject().field("standardunit", standardunitdatatype).field("factorunit", factorunitdatatype).field("label", label).field("value", value)
-							                        .endObject()).execute().actionGet();
+							      .setSource(jsonBuilder().startObject()
+							                              .field("standardunit", standardunitdatatype)
+							                              .field("factorunit", factorunitdatatype)
+							                              .field("label", label)
+							                              .field("value", value)
+							                              .endObject())
+							      .execute()
+							      .actionGet();
 						}
 					}
 				} else {
@@ -181,29 +193,34 @@ public class ESNode implements IndexerInterface {
 		in.close();
 		System.out.println("Data Entry complete");
 	}
-//TODO allow partial hits
+
+	// TODO allow partial hits
 	@Override
 	public SearchHit[] transportclient(String query, String path) {
 		/* Connecting the remote client with the central cluster */
 		Client clientremote = this.node.client();
 
 		/* Building the Query */
-		MatchQueryBuilder qb = QueryBuilders.matchQuery("label", query).analyzer("my_ngram_analyzer");
-		SearchRequestBuilder srb = clientremote.prepareSearch(path).setTypes("mappings");
-		SearchResponse retrieved = srb.setQuery(qb).execute().actionGet();
+		MatchQueryBuilder qb = QueryBuilders.matchQuery("label", query)
+		                                    .analyzer("my_ngram_analyzer");
+		SearchRequestBuilder srb = clientremote.prepareSearch(path)
+		                                       .setTypes("mappings");
+		SearchResponse retrieved = srb.setQuery(qb)
+		                              .execute()
+		                              .actionGet();
 
 		/* Retrieving the results from the query */
-		SearchHit[] results = retrieved.getHits().getHits();
+		SearchHit[] results = retrieved.getHits()
+		                               .getHits();
 		return results;
 	}
 
 	@Override
 	public void closeBulkLoader() {
-		if(bulkProcessor!=null)
-		bulkProcessor.close();
+		if (bulkProcessor != null)
+			bulkProcessor.close();
 	}
 
-	@Override
 	public void closeClient() {
 		client.close();
 	}
@@ -213,8 +230,15 @@ public class ESNode implements IndexerInterface {
 		List<Map<String, Object>> esData = new ArrayList<Map<String, Object>>();
 		SearchResponse response = null;
 		int i = 0;
-		while (response == null || response.getHits().hits().length != 0) {
-			response = client.prepareSearch("dbpedialabels").setTypes("mappings").setQuery(QueryBuilders.matchAllQuery()).setSize(scrollSize).setFrom(i * scrollSize).execute().actionGet();
+		while (response == null || response.getHits()
+		                                   .hits().length != 0) {
+			response = client.prepareSearch("dbpedialabels")
+			                 .setTypes("mappings")
+			                 .setQuery(QueryBuilders.matchAllQuery())
+			                 .setSize(scrollSize)
+			                 .setFrom(i * scrollSize)
+			                 .execute()
+			                 .actionGet();
 			for (SearchHit hit : response.getHits()) {
 				esData.add(hit.getSource());
 			}
