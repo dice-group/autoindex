77a5d4549d02cd0290b1ea01cbcb26f4dfcdc915
Kunal-Jha <kunal94jha@gmail.com>
2017-09-13 11:19:00 -0500

    UI updated

--- src/main/java/org/aksw/simba/autoindex/es/web/SearchResourceController.java
+++ src/main/java/org/aksw/simba/autoindex/es/web/SearchResourceController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+
 @RestController
 @RequestMapping("/rest/search")
 public class SearchResourceController {
