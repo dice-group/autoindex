ec4766c5f684fb447fbb54a5c05c23cdd8793b25
Kunal-Jha <kunal94jha@gmail.com>
2017-09-14 13:16:10 -0500

    Added prefix search to the label

--- src/main/java/org/aksw/simba/autoindex/es/resource/SearchResource.java
+++ src/main/java/org/aksw/simba/autoindex/es/resource/SearchResource.java
@@ -6,6 +6,10 @@ import java.util.List;
 import org.aksw.simba.autoindex.es.model.Entity;
 import org.aksw.simba.autoindex.es.repository.EntityRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
+import static org.elasticsearch.index.query.QueryBuilders.regexpQuery;
+import static org.elasticsearch.index.query.QueryBuilders.prefixQuery;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,13 +23,17 @@ public class SearchResource {
 	EntityRespository entityRepo;
 
 	@GetMapping(value = "/url/{text}")
-	public List<Entity> searchName(@PathVariable final String text) {
-		return entityRepo.findByUrl(text);
+	public List<Entity> searchUrl(@PathVariable final String text) {
+		SearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(regexpQuery("url", "http:.*" + text))
+				.build();
+
+		return entityRepo.search(searchQuery).getContent();
 	}
 
 	@GetMapping(value = "/label/{label}")
-	public List<Entity> searchSalary(@PathVariable final String label) {
-		return entityRepo.findByLabel(label);
+	public List<Entity> searchLabel(@PathVariable final String label) {
+		SearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(prefixQuery("label", label)).build();
+		return entityRepo.search(searchQuery).getContent();
 	}
 
 	@GetMapping(value = "/all")
