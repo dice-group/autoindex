17ad625df162a4f950c5914058b09823b5f8dde3
Prashanth <prasanthhs@yahoo.com>
2018-04-14 12:43:36 +0200

    Search Functionality Implementation
    1. Search Functionality.
    
    2. Dependency Injection for Search
    
    3. Remove unused files
    
    4. Version update to Pom.xml and removal of unnecessary dependancies.
    
    5. Bug fixes.

--- src/main/java/org/aksw/simba/autoindex/es/repository/EntityRepository.java
+++ src/main/java/org/aksw/simba/autoindex/es/repository/EntityRepository.java
@@ -1,6 +1,5 @@
 package org.aksw.simba.autoindex.es.repository;
 
-import static org.elasticsearch.index.query.QueryBuilders.prefixQuery;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,21 +8,33 @@ import java.util.List;
 import org.aksw.simba.autoindex.datasource.file.FileHandler;
 import org.aksw.simba.autoindex.datasource.sparql.SparqlHandler;
 import org.aksw.simba.autoindex.es.model.DataClass;
-//import org.aksw.simba.autoindex.es.config.ConfigBean;
 import org.aksw.simba.autoindex.es.model.Entity;
 import org.aksw.simba.autoindex.es.model.Property;
 import org.aksw.simba.autoindex.request.Request;
 import org.aksw.simba.autoindex.request.Request.RequestType;
+import org.aksw.simba.autoindex.request.SearchRequest;
+import org.aksw.simba.autoindex.request.SearchRequest.Category;
+import org.aksw.simba.autoindex.request.SearchRequest.Type;
+import org.aksw.simba.autoindex.response.Bindings;
+import org.aksw.simba.autoindex.response.Head;
 import org.aksw.simba.autoindex.response.Response;
+import org.aksw.simba.autoindex.response.Results;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
+
 @Service
 public class EntityRepository{
+	
+	private static final String categoryClass = "class";
+	private static final String categoryProperty = "property";
+	private static final String categoryEntity = "entity";
 	
 	@Autowired
 	ElasticSearchRepositoryInterface elasticSearchRepositoryInterface;
@@ -38,10 +49,86 @@ public class EntityRepository{
 		elasticSearchRepositoryInterface.save(list);
 	}
 	
-	public List<Entity> search(String type, String label) {
-		SearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(prefixQuery("label", label)).build();
-		return elasticSearchRepositoryInterface.search(searchQuery).getContent();
+	public Response addHead(Response response) {
+		Head head = response.getHead();
+		head.add("label");
+		head.add("uri");
+		response.setHead(head);
+		return response;
+	}
+	
+	public Response createResponse(List<Entity> entityList) {
+		Response response=new Response();
+		response.setBoolean(true);
+		response = addHead(response);
 		
+		Results results = response.getResults();
+		log.warn("Length of output List=" + entityList.size());
+		for (Entity entity : entityList) {
+			Bindings bindings = new Bindings();
+			bindings.setLabel(entity.getLabel());
+			bindings.setUri(entity.getUrl());
+			results.addBinding(bindings);
+		}
+		response.setResults(results);
+		return response;
+	}
+	
+	public String getCategory(Category category) {
+		String returnString = "";
+		switch(category) {
+		case CLASS:{
+			return categoryClass;
+		}
+		case PROPERTY :{
+			return categoryProperty;
+		}
+		case ENTITY :{
+			return categoryEntity;
+		}
+		default:
+			break;
+		}
+		return returnString;
+	}
+	
+	public String getType(Type type) {
+		String strType = "_all";
+		switch(type) {
+		case LABEL:{
+			strType = "label";
+			break;
+		}
+		case URI:{
+			strType = "url";
+			break;
+		}
+		default:
+			break;
+		}
+		return strType;
+	}
+	
+	public Response search(SearchRequest searchRequest) {	
+		//TODO: Add exception Handling and generate Output Response accordingly. 
+		//JSON must be sent at all times and not a 500 error
+		String query = searchRequest.getQuery();
+		Type type = searchRequest.getType();
+		Category category = searchRequest.getCategory();
+		NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+		if(category.equals(Category.NONE) || type.equals(Type.NONE) || query.isEmpty()) {
+			throw new IllegalArgumentException("Invalid Category or type or empty Query");
+		}
+		String strCategory = getCategory(category); 
+		nativeSearchQueryBuilder.withIndices(strCategory);
+		nativeSearchQueryBuilder.withTypes(strCategory);
+		
+		String strType = getType(type);
+		nativeSearchQueryBuilder.withQuery(QueryBuilders.matchQuery(strType , query)).withPageable(new PageRequest(0, 100));
+		
+		SearchQuery searchQuery = nativeSearchQueryBuilder.build();
+		List<Entity> entityList = elasticSearchRepositoryInterface.search(searchQuery).getContent();
+		return createResponse(entityList);
 	}
 	
 	public List<Entity> findall(){
