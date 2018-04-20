e7c04998554d7bee348de2c51f5841dcf0f49c9d
idreestahir <zmidrees@gmail.com>
2018-04-20 13:57:45 +0200

    Visual creation and field validations of labeled url section

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -19,7 +19,7 @@ $("#btn_submit").click(function(){
 		contentType: "application/json",
 		async: true,		
 	})
-	.done(function(data){
+	.done(function(data){	
 		alert("Indexing done");
 	})
 	.fail(function(data){
@@ -49,3 +49,26 @@ $("#btn_submitfile").click(function(){
 		alert("Error occured");
 	});
 });
+
+$("#submit_labeled_url").click(function(){
+	var url_validation = false;
+	var data = {};
+	var labeled_url_field = "#labeled_index";
+	var lebeled_url = $(labeled_url_field).val();
+    var is_url_valid = validate(lebeled_url);
+    if(is_url_valid == true){
+    $(this).parent().find('#labeled_index').removeClass('alert-danger');
+    console.log("Post Funcationality");
+    }
+    else{
+    $(this).parent().find('#labeled_index').addClass('alert-danger');
+    }
+	
+});
+    function validate(lebeled_url) {
+        var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+        if (pattern.test(lebeled_url)) {
+            return true;
+        } 
+            return false;
+    }
