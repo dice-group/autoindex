d468fcf383f2865268aa274efa63a8b23a8c8498
Prashanth <prasanthhs@yahoo.com>
2018-05-08 13:27:54 +0200

    Code update for Customizing Where clause
    1. New properties file to support flexible Where clause handling and also prefixes which can be as long as the user requires.
    2. Post construct to handle reading from property file.

--- src/main/java/org/aksw/simba/autoindex/datasource/sparql/SparqlHandler.java
+++ src/main/java/org/aksw/simba/autoindex/datasource/sparql/SparqlHandler.java
@@ -3,21 +3,22 @@
  */
 package org.aksw.simba.autoindex.datasource.sparql;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.annotation.PostConstruct;
 
 import org.aksw.simba.autoindex.es.model.DataClass;
 import org.aksw.simba.autoindex.es.model.Entity;
 import org.aksw.simba.autoindex.es.model.Property;
-import org.aksw.simba.autoindex.request.Keys;
 import org.aksw.simba.autoindex.request.Request;
 import org.apache.jena.query.ParameterizedSparqlString;
 import org.apache.jena.query.Query;
@@ -27,67 +28,49 @@ import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 @Component
 public class SparqlHandler {
 	
 	private static final Logger log = LoggerFactory
 	            .getLogger(SparqlHandler.class);
-
-	private static final String commandString = "SELECT DISTINCT ?key1 ?key2 WHERE{?key1 a ?s1 . ?key1 ?s2 ?key2 .}" ;
+	private static  String commandString = "" ; //Read from properties file.
+	private static  String propertiesString = "" ;
+	private static  String classesString = "";
+	public static  Map<String, String> prefixMap;
+	private static final String TEMPLATE_FILE = "src/main/resources/application.properties";
+	private Properties properties = new Properties();
 	
-	private static final String propertiesString = "SELECT DISTINCT ?key1 ?key2  (COUNT(*)AS ?v) WHERE {\n" + 
-			"		?key1 a rdf:Property;\n" + 
-			"		?s2 ?key2.\n" + 
-			"		} GROUP BY ?key1 ?key2 \n" + 
-			"		 ORDER BY DESC(?v)" ;
-	
-	private static final String classesString = "SELECT DISTINCT ?key1 ?key2  \n" + 
-			"		WHERE {\n" + 
-			"		?key1 a owl:Class .\n" + 
-			"		?key1 ?s2 ?key2 .\n" + 
-			"		}";
-	public static final Map<String, String> prefixMap;
-	
-	static {
-		 	Map<String, String> prefix = new HashMap<String, String>();
-		    	prefix.put("owl", "http://www.w3.org/2002/07/owl#");
-		    	prefix.put("xsd", "http://www.w3.org/2001/XMLSchema#");
-		    	prefix.put("rdf","http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-		    	prefix.put("rdfs","http://www.w3.org/2000/01/rdf-schema#");
-		    	prefix.put("vrank","http://purl.org/voc/vrank#");
-		    	prefixMap = Collections.unmodifiableMap(prefix);
-		    };
-	
-	private static final String TEMPLATE_FILE = "src/main/resources/properties/properties.yml";
-	
-	private  static String key1 ;
-
-	private static String key2; 
-	//Used in addition of @PropertySource
-
 	@PostConstruct
-	private void resourceLoader() {
+	private void resourceLoader() throws FileNotFoundException {
+		InputStream input = null;
 		try {
-			
-			String yamlTemplateContents = new String(Files.readAllBytes(Paths.get(TEMPLATE_FILE)));
-			ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
-			Keys keys = new Keys();
-			keys = yamlReader.readValue(yamlTemplateContents, Keys.class);
-			
-			key1 = keys.getFirstKey();
-			key2 = keys.getSecondKey();
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			log.error("resourceLoader, IO Exception while parsing YAML template,Stack Trace=" + e.getMessage());
-			e.printStackTrace();
+			 input = new FileInputStream(TEMPLATE_FILE);
+			 properties.load(input);
+			 commandString = properties.getProperty("entity.whereclause");
+			 propertiesString = properties.getProperty("property.whereclause");
+			 classesString = properties.getProperty("class.whereclause");
+			 int i=1;
+			 Map<String, String> prefix = new HashMap<String, String>();
+			 while(properties.containsKey("prefix" + i + ".name")) {
+				 prefix.put(properties.getProperty("prefix" + i + ".name") , properties.getProperty("prefix" + i + ".url"));
+				 ++i;
+			 }
+			 prefixMap = Collections.unmodifiableMap(prefix);
+			 System.out.println(prefixMap.toString());
+		}
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}	
+		finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 	
@@ -152,8 +135,7 @@ public class SparqlHandler {
     		//TODO: Find a way to handle this by ParameterizedSparql String. 
     		// Doesnt work with SetLiteral or SetParam(Node) or setIRI. Try other options
     		String commandText = commandString;
-    		commandText= commandText.replace("?s1", key1);
-    		commandText=commandText.replace("?s2", key2);
+    	
     		sparqlQueryHandler.setCommandText(commandText);
     		
     		if(!defaultGraph.isEmpty()) {
