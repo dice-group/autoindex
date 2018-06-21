14529d4025cb03cc7ee794e220775a012a536274
idreestahir <zmidrees@gmail.com>
2018-06-21 17:20:18 +0500

    resolving mixed spaces and tabs

--- src/main/resources/static/js/query.js
+++ src/main/resources/static/js/query.js
@@ -1,3 +1,22 @@
+function renderTable(data){
+	var obj = JSON.parse(data);
+	if(obj.boolean === false){
+		alert("Search Failed");
+	}
+	else{
+	var tableData = "<table align=\"center\"><tr><th>LABEL</th><th>URL</th></tr>";	
+	for (var i = 0; i < obj.results.bindings.length; i++) {
+		var counter = obj.results.bindings[i];
+		var temp = "<tr>";
+		temp = "<td>" + counter.label.value + "</td>";
+		temp += "<td>" + counter.uri.value + "</td>";
+		temp += "</tr>";
+		tableData += temp;
+	}
+	tableData += "</table><h1 align=\"center\">End of Results</h1>";
+	$("#results").html(tableData);
+	}
+}
 $("#searchButtonLabel").click(function(){
 	var label = $("#queryText").val();
 	var data = {};
@@ -17,25 +36,6 @@ $("#searchButtonLabel").click(function(){
 		renderTable(data);
 	});
 });
-function renderTable(data){
-	var obj = JSON.parse(data);
-	if(obj.boolean === false){
-		alert("Search Failed");
-	}
-	else{
-	var tableData = "<table align=\"center\"><tr><th>LABEL</th><th>URL</th></tr>";	
-	for (var i = 0; i < obj.results.bindings.length; i++) {
-		var counter=obj.results.bindings[i];
-	    var temp="<tr>";
-	    temp="<td>" + counter.label.value + "</td>";
-	    temp+="<td>" + counter.uri.value + "</td>";
-	    temp+="</tr>";
-	    tableData+=temp;
-	}
-	tableData += "</table><h1 align=\"center\">End of Results</h1>";
-	$("#results").html(tableData);
-	}
-}
 $("input[name=options]").change(function () {
 	if(this.value === "uri"){
 		$("#queryText").attr("placeholder", "Enter Query URL");
