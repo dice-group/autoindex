5bdc9a992de4c1188695ffece88c953b9a5d1c95
prasanthhs <prasanthhs@yahoo.com>
2018-06-29 13:25:10 +0200

    Draft Version of Elastic search setup for Unit tests

---
+++ src/test/java/org/aksw/simba/autoindex/es/EntityRepositoryTest.java
@@ -0,0 +1,62 @@
+package org.aksw.simba.autoindex.es;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import org.apache.commons.io.FileUtils;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+
+public class EntityRepositoryTest {
+	
+	  @Inject
+	  private static ElasticsearchTemplate elasticSearchTemplate;
+	  
+	  private static Client client_;
+	  private static Node node_;
+	  private static String storagePath_ = "./elasticTests";
+	
+	
+	@BeforeClass
+	public static void Setup() throws IOException {
+		Settings.Builder elasticsearchSettings = Settings.settingsBuilder()
+			      .put("http.enabled", "false")
+			      .put("path.home", storagePath_);
+
+			    node_ = new NodeBuilder()
+			      .local(true)
+			      .settings(elasticsearchSettings.build())
+			      .node();
+
+			    client_ = node_.client();
+			    elasticSearchTemplate = new ElasticsearchTemplate(client_);
+	}
+	@Test
+	public void Test1() {
+		
+	}
+	@AfterClass
+	public static void tearDown() throws IOException {
+		node_.close();
+		deleteStorage();
+	}
+	
+	public static void deleteStorage() throws IOException
+	  {
+	    File storage = new File(storagePath_);
+
+	    if(storage.exists())
+	    {
+	      FileUtils.deleteDirectory(storage);
+	    }
+
+	  }
+}
